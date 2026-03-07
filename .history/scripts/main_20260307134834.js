5a745b7ae0eed176f1c8591aa7d782ead596feb6
Chardonne Dixon <chardonne.dixon@gmail.com>
2026-03-07 13:55:58 -0600

    Implemented main algorithm in main module

--- .history/scripts/main_20260307134834.js
+++ .history/scripts/main_20260307134834.js
@@ -1,4 +1,2 @@
 import { generateMoviesHTML } from './movie-converter.js';
 import { renderMoviesToDOM } from './movie-render.js';
-const movieHTML = generateMoviesHTML();
-renderMoviesToDOM(movieHTML);
--- .history/scripts/main_20260307134845.js
+++ .history/scripts/main_20260307134845.js
@@ -1,4 +1,3 @@
 import { generateMoviesHTML } from './movie-converter.js';
 import { renderMoviesToDOM } from './movie-render.js';
 const movieHTML = generateMoviesHTML();
-renderMoviesToDOM(movieHTML);
